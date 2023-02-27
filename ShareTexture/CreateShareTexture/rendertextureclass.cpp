fe48b0ddaa659137919927725a805a0cb869e3f5
yangkemin <iherewaitfor@163.com>
2023-02-27 22:42:08 +0800

    add ShareTexture/CreateShareTexture

---
+++ ShareTexture/CreateShareTexture/rendertextureclass.cpp
@@ -0,0 +1,153 @@
+#include "rendertextureclass.h"
+
+RenderTextureClass::RenderTextureClass()
+{
+	m_renderTargetTexture = 0;
+	m_renderTargetView = 0;
+	m_shaderResourceView = 0;
+	m_hsharedHandle = NULL;
+	m_pDXGIKeyedMutex = NULL;
+}
+
+
+RenderTextureClass::RenderTextureClass(const RenderTextureClass& other)
+{
+}
+
+
+RenderTextureClass::~RenderTextureClass()
+{
+}
+
+
+bool RenderTextureClass::Initialize(ID3D11Device* device, int textureWidth, int textureHeight)
+{
+	D3D11_TEXTURE2D_DESC textureDesc;
+	HRESULT result;
+	D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+
+
+	//创建2d纹理，当作渲染target
+	ZeroMemory(&textureDesc, sizeof(textureDesc));
+
+	textureDesc.Width = textureWidth;
+	textureDesc.Height = textureHeight;
+	textureDesc.MipLevels = 1;
+	textureDesc.ArraySize = 1;
+	textureDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	textureDesc.SampleDesc.Count = 1;
+	textureDesc.Usage = D3D11_USAGE_DEFAULT;
+	textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	textureDesc.CPUAccessFlags = 0;
+    textureDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX;
+
+	result = device->CreateTexture2D(&textureDesc, NULL, &m_renderTargetTexture);
+	if(FAILED(result))
+	{
+		return false;
+	}
+
+	renderTargetViewDesc.Format = textureDesc.Format;
+	renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	renderTargetViewDesc.Texture2D.MipSlice = 0;
+
+	//创建渲染目标视图.
+	result = device->CreateRenderTargetView(m_renderTargetTexture, &renderTargetViewDesc, &m_renderTargetView);
+	if(FAILED(result))
+	{
+		return false;
+	}
+
+	// 创建shader资源，和纹理关联起来
+	shaderResourceViewDesc.Format = textureDesc.Format;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+
+	result = device->CreateShaderResourceView(m_renderTargetTexture, &shaderResourceViewDesc, &m_shaderResourceView);
+	if(FAILED(result))
+	{
+		return false;
+	}
+
+	// QI IDXGIResource interface to synchronized shared surface.
+	IDXGIResource* pDXGIResource = NULL;
+	m_renderTargetTexture->QueryInterface(__uuidof(IDXGIResource), (LPVOID*)&pDXGIResource);
+
+	// obtain handle to IDXGIResource object.
+	pDXGIResource->GetSharedHandle(&m_hsharedHandle);
+	pDXGIResource->Release();
+	DWORD lerror = 0;
+	if (!m_hsharedHandle)
+	{
+		lerror = GetLastError();
+	}
+	// QI IDXGIKeyedMutex interface of synchronized shared surface's resource handle.
+	result = m_renderTargetTexture->QueryInterface(__uuidof(IDXGIKeyedMutex),
+		(LPVOID*)&m_pDXGIKeyedMutex);
+	if(FAILED(result) || (m_pDXGIKeyedMutex == NULL))
+		return false;
+	return true;
+}
+
+
+void RenderTextureClass::Shutdown()
+{
+	if(m_shaderResourceView)
+	{
+		m_shaderResourceView->Release();
+		m_shaderResourceView = 0;
+	}
+
+	if(m_renderTargetView)
+	{
+		m_renderTargetView->Release();
+		m_renderTargetView = 0;
+	}
+
+	if(m_renderTargetTexture)
+	{
+		m_renderTargetTexture->Release();
+		m_renderTargetTexture = 0;
+	}
+
+	return;
+}
+
+
+void RenderTextureClass::SetRenderTarget(ID3D11DeviceContext* deviceContext, ID3D11DepthStencilView* depthStencilView)
+{
+	//绑定渲染目标视图和深度模版视图.
+	deviceContext->OMSetRenderTargets(1, &m_renderTargetView, depthStencilView);
+	
+	return;
+}
+
+
+void RenderTextureClass::ClearRenderTarget(ID3D11DeviceContext* deviceContext, ID3D11DepthStencilView* depthStencilView, 
+										   float red, float green, float blue, float alpha)
+{
+	float color[4];
+
+
+	// 设置渲染背景颜色
+	color[0] = red;
+	color[1] = green;
+	color[2] = blue;
+	color[3] = alpha;
+
+	// 清除渲染目标视图当前内容
+	deviceContext->ClearRenderTargetView(m_renderTargetView, color);
+    
+	// 复位深度模版视图
+	deviceContext->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	return;
+}
+
+
+ID3D11ShaderResourceView* RenderTextureClass::GetShaderResourceView()
+{
+	return m_shaderResourceView;
+}
---
+++ ShareTexture/CreateShareTexture/rendertextureclass.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <d3d11.h>
+
+class RenderTextureClass
+{
+public:
+	RenderTextureClass();
+	RenderTextureClass(const RenderTextureClass&);
+	~RenderTextureClass();
+
+	bool Initialize(ID3D11Device*, int, int);
+	void Shutdown();
+
+	void SetRenderTarget(ID3D11DeviceContext*, ID3D11DepthStencilView*);
+	void ClearRenderTarget(ID3D11DeviceContext*, ID3D11DepthStencilView*, float, float, float, float);
+	ID3D11ShaderResourceView* GetShaderResourceView();
+
+private:
+	ID3D11Texture2D* m_renderTargetTexture; //渲染目标视图
+	ID3D11RenderTargetView* m_renderTargetView;
+	ID3D11ShaderResourceView* m_shaderResourceView;//shader资源
+	HANDLE				m_hsharedHandle;
+public:
+	IDXGIKeyedMutex* m_pDXGIKeyedMutex;
+};
